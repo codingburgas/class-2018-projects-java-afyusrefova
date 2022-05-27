a79fb143cfd2da76a7cfe825c2c9ed335a96f364
afyusrefova <58163040+afyusrefova@users.noreply.github.com>
2022-05-27 10:02:29 +0300

    Delete NursingHome.sql

--- NursingHome.sql
+++
@@ -1,6 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [elder_id]
-      ,[dateOfBirth]
-      ,[first_name]
-      ,[last_name]
-  FROM [nursingHome].[dbo].[Elders]
